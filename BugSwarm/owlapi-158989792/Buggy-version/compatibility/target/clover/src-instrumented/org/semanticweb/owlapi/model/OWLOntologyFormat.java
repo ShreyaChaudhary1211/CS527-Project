2429859b7ef3af44075d2b3210b77286698e9157
ShreyaChaudhary1211 <158503157+ShreyaChaudhary1211@users.noreply.github.com>
2024-04-30 18:41:08 -0500

    owl

--- BugSwarm/owlapi-158989792/Buggy-version/compatibility/target/clover/src-instrumented/org/semanticweb/owlapi/model/OWLOntologyFormat.java
+++
@@ -1,5 +0,0 @@
-/* $$ This file has been instrumented by Clover 4.5.2#20240131180750 $$ */package org.semanticweb.owlapi.model;
-
-/** @deprecated this interface is here only to help with backwards compatibility */
-@Deprecated
-public interface OWLOntologyFormat extends OWLDocumentFormat {}
--- BugSwarm/owlapi-158989792/Buggy-version/parsers/target/clover/src-instrumented/org/semanticweb/owlapi/rdf/rdfxml/parser/BuiltInTypeHandler.java
+++
@@ -1,8 +0,0 @@
-/* $$ This file has been instrumented by Clover 4.5.2#20240131180750 $$ */package org.semanticweb.owlapi.rdf.rdfxml.parser;
-
-import org.semanticweb.owlapi.model.IRI;
-
-interface BuiltInTypeHandler extends TriplePredicateHandler {
-
-    IRI getTypeIRI();
-}
