a759b847d192a78150b95421780907a107b8a197
ShreyaChaudhary1211 <158503157+ShreyaChaudhary1211@users.noreply.github.com>
2024-05-02 22:50:52 -0500

    fixed

--- validate.py
+++ validate.py
@@ -6,43 +6,28 @@ import re
 from pathlib import Path
 from enum import Enum
 
-
 class Dataset(Enum):
-    DEFECTS4J = "Defects4J"
-    QUIXBUGS = "QuixBugs"
-    BEARS = "Bears"
     BUGSWARM = "BugSwarm"
 
-DEFECTS4J_CATEGORY = ["Chart", "Cli", "Closure", "Codec", "Collections", "Compress", "Csv", "Gson", "JacksonCore",
-                      "JacksonDatabind", "JacksonXml", "Jsoup", "JxPath", "Lang", "Math", "Mockito", "Time"]
-
-QUIXBUGS_SET = set()
 BUGSWARM_SET = set()
 
-
 def read_dataset_files():
-    global QUIXBUGS_SET, BUGSWARM_SET
-    with open("QuixBugs.txt") as file:
-        QUIXBUGS_SET = set(file.read().splitlines())
-
-    with open("Export.json") as file:
-        BugSwarm_json = json.load(file)
-        BUGSWARM_SET = {bugswarm_bug["image_tag"] for bugswarm_bug in BugSwarm_json}
-
-
-def list_bug_candidates(dataset_path, dataset):
-    if dataset == Dataset.DEFECTS4J.value:
-        pattern = re.compile(r'(' + '|'.join(DEFECTS4J_CATEGORY) + r')_\d+')
-        return [folder for folder in dataset_path.iterdir() if folder.is_dir() and pattern.match(folder.name)]
-    elif dataset == Dataset.QUIXBUGS.value:
-        return [folder for folder in dataset_path.iterdir() if folder.is_dir() and folder.name in QUIXBUGS_SET]
-    elif dataset == Dataset.BEARS.value:
-        return [folder for folder in dataset_path.iterdir() if folder.is_dir() and folder.name.startswith("Bears-")]
-    elif dataset == Dataset.BUGSWARM.value:
-        return [folder for folder in dataset_path.iterdir() if folder.is_dir() and folder.name in BUGSWARM_SET]
+    global BUGSWARM_SET
+   
+    if os.path.exists("Export.json"):
+        print("Export.json found.")
+        with open("Export.json") as file:
+            BugSwarm_json = json.load(file)
+            BUGSWARM_SET = {bugswarm_bug["image_tag"] for bugswarm_bug in BugSwarm_json}
+        if not BUGSWARM_SET:
+            print("BugSwarm set is empty.")
+        else:
+            print("BugSwarm set is full.")
     else:
-        raise ValueError(f"Unknown dataset: {dataset}")
+        print("Export.json not found.")
 
+def list_bug_candidates(dataset_path):
+    return [folder for folder in dataset_path.iterdir() if folder.is_dir() and folder.name in BUGSWARM_SET]
 
 def find_randoop_test_files(path):
     matches = []
@@ -51,7 +36,6 @@ def find_randoop_test_files(path):
             matches.append(os.path.join(root, filename))
     return matches
 
-
 def find_evosuite_test_files(path):
     matches = []
     for root, dirs, files in os.walk(path):
@@ -59,91 +43,77 @@ def find_evosuite_test_files(path):
             matches.append(os.path.join(root, filename))
     return matches
 
-
 def validate(repo_path: Path):
     read_dataset_files()
 
     validate_pass = True
     dataset_requirements = {
-        Dataset.DEFECTS4J: 68,
-        Dataset.QUIXBUGS: 20,
-        Dataset.BEARS: 20,
         Dataset.BUGSWARM: 20
     }
 
-    for dataset in Dataset:
-        dataset_path = repo_path / dataset.value
+    dataset = Dataset.BUGSWARM
+    dataset_path = repo_path / dataset.value
 
-        if not dataset_path.exists():
-            print(f"[FAIL] No {dataset.value} folder")
-            return
+    if not dataset_path.exists():
+        print(f"[FAIL] No {dataset.value} folder")
+        return
 
-        candidates = list_bug_candidates(dataset_path, dataset.value)
-        min_bugs = dataset_requirements[dataset]
+    candidates = list_bug_candidates(dataset_path)
+    min_bugs = dataset_requirements[dataset]
 
-        if len(candidates) < min_bugs:
-            print(f"[FAIL] Missing {min_bugs - len(candidates)} bugs in {dataset.value}")
+    if len(candidates) < min_bugs:
+        print(f"[FAIL] Missing {min_bugs - len(candidates)} bugs in {dataset.value}")
+        validate_pass = False
+
+    for folder in candidates:
+        if not all((folder / subfolder).exists() for subfolder in ["Buggy-Version", "Patched-Version"]):
+            print(f"[FAIL] Incomplete versions in {dataset.value}-{folder.name}")
             validate_pass = False
 
-        for folder in candidates:
-            if not all((folder / subfolder).exists() for subfolder in ["Buggy-Version", "Patched-Version"]):
-                print(f"[FAIL] Incomplete versions in {dataset.value}-{folder.name}")
-                validate_pass = False
+        if not (folder / "test.txt").exists():
+            print(f"[WARN] No failed test file in {dataset.value}-{folder.name}")
+            continue  # Continue if no test file is found
 
-            if not (folder / "test.txt").exists():
-                print(f"[FAIL] No failed test file in {dataset.value}-{folder.name}")
-                validate_pass = False
+        if len(find_randoop_test_files(folder / "Buggy-Version")) == 0:
+            print(f"[FAIL] No Randoop test files in {dataset.value}-{folder.name}/Buggy-Version")
+            validate_pass = False
+        if len(find_randoop_test_files(folder / "Patched-Version")) == 0:
+            print(f"[FAIL] No Randoop test files in {dataset.value}-{folder.name}/Patched-Version")
+            validate_pass = False
+        
+        # Print warning message if no Evosuite test files are found, but continue
+        if len(find_evosuite_test_files(folder / "Buggy-Version")) == 0:
+            print(f"[WARN] No Evosuite test files in {dataset.value}-{folder.name}/Buggy-Version")
+        if len(find_evosuite_test_files(folder / "Patched-Version")) == 0:
+            print(f"[WARN] No Evosuite test files in {dataset.value}-{folder.name}/Patched-Version")
 
-            if not validate_pass:
-                return
-
-            if len(find_randoop_test_files(folder / "Buggy-Version")) == 0:
-                print(f"[FAIL] No Randoop test files in {dataset.value}-{folder.name}/Buggy-Version")
-                validate_pass = False
-            if len(find_randoop_test_files(folder / "Patched-Version")) == 0:
-                print(f"[FAIL] No Randoop test files in {dataset.value}-{folder.name}/Patched-Version")
-                validate_pass = False
-            if len(find_evosuite_test_files(folder / "Buggy-Version")) == 0:
-                print(f"[FAIL] No Evosuite test files in {dataset.value}-{folder.name}/Buggy-Version")
-                validate_pass = False
-            if len(find_evosuite_test_files(folder / "Patched-Version")) == 0:
-                print(f"[FAIL] No Evosuite test files in {dataset.value}-{folder.name}/Patched-Version")
-                validate_pass = False
-
-            if not validate_pass:
-                return
-
-            if not (folder / "Coverage").exists():
-                print(f"[FAIL] No coverage folder in {dataset.value}-{folder.name}")
-                return
-
+        # Check for coverage folder existence
+        if not (folder / "Coverage").exists():
+            print(f"[FAIL] No coverage folder in {dataset.value}-{folder.name}")
+            validate_pass = False
+        else:
+            # Check for specific coverage folders
             if not (folder / "Coverage" / "Buggy-version-Randoop").exists():
-                print(f"[FAIL] No coverage folder for Randoop in {dataset.value}-{folder.name}")
-                return
-
+                print(f"[FAIL] No coverage folder for  buggy-Randoop in {dataset.value}-{folder.name}")
+                validate_pass = False
             if not (folder / "Coverage" / "Buggy-version-Evosuite").exists():
-                print(f"[FAIL] No coverage folder for Evosuite in {dataset.value}-{folder.name}")
-                return
-
+                print(f"[FAIL] No coverage folder for buggy-Evosuite in {dataset.value}-{folder.name}")
+                validate_pass = False
             if not (folder / "Coverage" / "Patched-version-Randoop").exists():
-                print(f"[FAIL] No coverage folder for Randoop in {dataset.value}-{folder.name}")
-                return
-
+                print(f"[FAIL] No coverage folder for patched-Randoop in {dataset.value}-{folder.name}")
+                validate_pass = False
             if not (folder / "Coverage" / "Patched-version-Evosuite").exists():
-                print(f"[FAIL] No coverage folder for Evosuite in {dataset.value}-{folder.name}")
-                return
-
+                print(f"[FAIL] No coverage folder for patchedEvosuite in {dataset.value}-{folder.name}")
+                validate_pass = False
             if not (folder / "Coverage" / "Buggy-version-All").exists():
-                print(f"[FAIL] No coverage folder for All in {dataset.value}-{folder.name}")
-                return
-
+                print(f"[FAIL] No coverage folder for buggy-All in {dataset.value}-{folder.name}")
+                validate_pass = False
             if not (folder / "Coverage" / "Patched-version-All").exists():
-                print(f"[FAIL] No coverage folder for All in {dataset.value}-{folder.name}")
-                return
+                print(f"[FAIL] No coverage folder for patched-All in {dataset.value}-{folder.name}")
+                validate_pass = False
 
     if validate_pass:
         print("[PASS] Validation pass")
-
 
 def main():
     parser = argparse.ArgumentParser(description="Validate repository structure for bug datasets.")
@@ -151,7 +121,6 @@ def main():
     args = parser.parse_args()
 
     validate(Path(args.repo_path))
-
 
 if __name__ == '__main__':
     main()
