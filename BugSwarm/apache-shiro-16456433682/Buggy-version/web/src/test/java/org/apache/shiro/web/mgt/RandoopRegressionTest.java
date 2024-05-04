eda7650cd43cf56b08b2a1ab5a3c41acd0ea8753
ShreyaChaudhary1211 <158503157+ShreyaChaudhary1211@users.noreply.github.com>
2024-05-03 22:25:27 -0500

    apache-shiro-16456433682

--- BugSwarm/apache-shiro-16456433682/Buggy-version/web/src/test/java/org/apache/shiro/web/mgt/RandoopRegressionTest.java
+++
@@ -1,9 +0,0 @@
-package org.apache.shiro.web.mgt;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ RandoopRegressionTest0.class })
-public class RandoopRegressionTest {
-}
-
