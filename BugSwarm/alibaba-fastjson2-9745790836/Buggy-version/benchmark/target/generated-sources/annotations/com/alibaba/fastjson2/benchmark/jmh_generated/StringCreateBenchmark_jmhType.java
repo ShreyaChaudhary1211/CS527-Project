0e0bdd6e66f08f64393017b2ddcfdcc9e48d1d88
ShreyaChaudhary1211 <158503157+ShreyaChaudhary1211@users.noreply.github.com>
2024-05-03 19:20:06 -0500

    alibaba-fastjson2-9745790836

--- BugSwarm/alibaba-fastjson2-9745790836/Buggy-version/benchmark/target/generated-sources/annotations/com/alibaba/fastjson2/benchmark/jmh_generated/StringCreateBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.alibaba.fastjson2.benchmark.jmh_generated;
-public class StringCreateBenchmark_jmhType extends StringCreateBenchmark_jmhType_B3 {
-}
-
--- BugSwarm/alibaba-fastjson2-9745790836/Buggy-version/benchmark/target/generated-sources/annotations/com/alibaba/fastjson2/benchmark/primitves/jmh_generated/FloatValue20_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.alibaba.fastjson2.benchmark.primitves.jmh_generated;
-public class FloatValue20_jmhType extends FloatValue20_jmhType_B3 {
-}
-
